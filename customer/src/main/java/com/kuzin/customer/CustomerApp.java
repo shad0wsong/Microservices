36dc2146eb393ae8741374c36b83a28e7ec44632
Dan Kuzin <shadows_song@mail.ru>
2022-08-31 21:49:32 +0400

    zipkin and docker-compose

--- customer/src/main/java/com/kuzin/customer/CustomerApp.java
+++ customer/src/main/java/com/kuzin/customer/CustomerApp.java
@@ -1,11 +1,13 @@
 package com.kuzin.customer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableAutoConfiguration
 public class CustomerApp {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApp.class,args);
--- fraud/src/main/java/com/kuzin/fraud/FraudApp.java
+++ fraud/src/main/java/com/kuzin/fraud/FraudApp.java
@@ -1,11 +1,13 @@
 package com.kuzin.fraud;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableAutoConfiguration
 public class FraudApp {
     public static void main(String[] args) {
         SpringApplication.run(FraudApp.class,args);
